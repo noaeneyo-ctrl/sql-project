7c6aeb5c4105f53b4b96e2c955c4ad857dbf508a
noaeneyo <noaeneyo@hilma.tech>
2025-12-01 09:59:26 +0200

    get user posts

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,27 @@
 var express = require("express");
 var router = express.Router();
-const { getAllPosts } = require("../services/posts_serv");
+const { getAllPosts, getUserPosts } = require("../services/posts_serv");
 
 router.get("/", async (req, res) => {
   try {
     const posts = await getAllPosts();
+    res.json(posts);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+router.get("/:id", async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const posts = await getUserPosts(userId);
+
+    if (!posts || posts.length === 0) {
+      return res.status(404).json({
+        message: `User with ID ${userId} does not exist or has no posts.`,
+      });
+    }
+
     res.json(posts);
   } catch (err) {
     res.status(500).json({ error: err.message });
