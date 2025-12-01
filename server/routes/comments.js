d4e42ff965d3e3f626fa90b2924c531789be8525
zivronsky-ui <zivronsky@hilma.tech>
2025-12-01 18:49:25 +0200

    only post again

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -2,13 +2,8 @@ var express = require("express");
 var router = express.Router();
 const { getPostsComments } = require("../services/comments_serv");
 
-<<<<<<< HEAD
-router.get("posts/:postId", async (req, res) => {
-  const postId = req.params.postId;
-=======
 router.get("/:post_id", async (req, res) => {
   const post_id = req.params.post_id;
->>>>>>> 8fec5d541a7b27ae19b3d7e644178568394fa63a
   try {
     const comments = await getPostsComments(post_id);
     if (!comments || comments.length === 0) {
